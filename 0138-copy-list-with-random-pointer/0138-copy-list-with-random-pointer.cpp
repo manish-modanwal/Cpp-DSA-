1e7e4f23df77363f4db58d7b759b2f22352c7094
Manish Modanwal <152703377+manish-modanwal@users.noreply.github.com>
2025-09-29 13:34:42 +0530

    Time: 12 ms (15.68%), Space: 15.3 MB (26.86%) - LeetHub

---
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -0,0 +1,44 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* helper(Node* head, unordered_map<Node*, Node*>&mp)
+    {
+        if(head==0)
+        {
+            return head;
+        }
+
+        Node* newnode= new Node(head->val);
+
+        mp[head]=newnode;
+        newnode->next=helper(head->next,mp);
+        if(head->random)
+        {
+            newnode->random=mp[head->random];
+        }
+
+        return newnode;
+
+
+    }
+    Node* copyRandomList(Node* head) 
+    {   unordered_map<Node*, Node*> mp;
+       return helper(head, mp);
+        
+    }
+};
