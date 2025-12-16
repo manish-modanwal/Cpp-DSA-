1e00632ff7e2121a48b85a4707ff2faeb146c918
Manish Modanwal <152703377+manish-modanwal@users.noreply.github.com>
2025-12-17 03:06:58 +0530

    Time: 4 ms (59.37%), Space: 28.3 MB (78.65%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& arr) {
         int n = arr.size();
-        vector<int> ans(n, -1);  // Initialize with -1
+        vector<int> ans(n, -1);  
         stack<int> st;
         
-        // Traverse from right to left, but twice
-        for(int i = 2*n - 1; i >= 0; i--) {  // i-- not i++
+        
+        for(int i = 2*n - 1; i >= 0; i--) {  
             int idx = i % n;
             
             while(!st.empty() && st.top() <= arr[idx]) {
@@ -20,7 +20,7 @@ public:
                 // else already -1 (from initialization)
             }
             
-            st.push(arr[idx]);  // Push current element
+            st.push(arr[idx]);  
         }
         
         return ans;
