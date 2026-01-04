f9b7b3c3781ecc5e713fbd5fec86ad8cf5d98bbb
Manish Modanwal <152703377+manish-modanwal@users.noreply.github.com>
2026-01-04 15:50:36 +0530

    Time: 10 ms (21.82%), Space: 11.9 MB (27.16%) - LeetHub

--- 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.cpp
+++ 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.cpp
@@ -1,23 +1,37 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> st;
 
-        for (char ch : s) {
+        stack<char>st;
+
+        for(auto ch :s)
+        {
             st.push(ch);
+            
+             char c;
+             char b;
+             char a;
+            if(st.size()>=3)
+            {
+                 c= st.top();
+                        st.pop();
+                 b= st.top();
+                        st.pop();  
+                 a= st.top();
+                        st.pop(); 
 
-            if (st.size() >= 3) {
-                char c = st.top(); st.pop();
-                char b = st.top(); st.pop();
-                char a = st.top(); st.pop();
-
-                if (!(a == 'a' && b == 'b' && c == 'c')) {
-                    st.push(a);
-                    st.push(b);
-                    st.push(c);
-                }
+               if(!(c=='c' && b=='b'  && a=='a'))
+            {
+                st.push(a);
+                st.push(b);
+                st.push(c);
+            }                      
             }
+
+            
         }
+
         return st.empty();
+        
     }
 };
