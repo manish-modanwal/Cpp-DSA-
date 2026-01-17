86fd08249bacca047137eb0c3b70544f51854686
Manish Modanwal <152703377+manish-modanwal@users.noreply.github.com>
2026-01-18 00:31:42 +0530

    Time: 0 ms (100%), Space: 15 MB (62.88%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -11,39 +11,24 @@
  */
 class Solution {
 public:
+    void dfs(TreeNode* root,int level, vector<int>& ans)
+    {
+        if (root == NULL) return;
+
+        if(level==ans.size())
+            ans.push_back(root->val);
+
+        dfs(root->right,level+1,ans);
+        dfs(root->left,level+1,ans);
+
+    }
     vector<int> rightSideView(TreeNode* root) {
-    vector<int> ans;
-    if (!root) return ans;
+        vector<int>ans;
 
-    vector<TreeNode*> v;
-    queue<TreeNode*> q;
+        dfs(root,0,ans);
 
-    q.push(root);
-    q.push(NULL);
+        return ans;
 
-    while (!q.empty()) {
-        TreeNode* node = q.front();
-        q.pop();
-
-        if (node == NULL) {
-            v.push_back(NULL);
-            if (!q.empty()) q.push(NULL);
-        } else {
-            v.push_back(node);
-            if (node->left) q.push(node->left);
-            if (node->right) q.push(node->right);
-        }
+        
     }
-
-    // traverse from back
-    for (int i = v.size() - 1; i >= 0; i--) {
-        if (v[i] == NULL && i > 0 && v[i - 1] != NULL) {
-            ans.push_back(v[i - 1]->val);
-        }
-    }
-
-    reverse(ans.begin(), ans.end());
-    return ans;
-}
-
 };
